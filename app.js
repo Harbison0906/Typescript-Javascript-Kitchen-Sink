4da3262a7add054e86fa4eab285b57a5a50f5ef5
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-07-29 21:31:08 -0500

    added sum variable and alert window function

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+//variable for my name//
 let myName = 'Seth';
+
+//constant for number of U.S. states//
 const states = 50;
+
+//variabe for sum of 5 and 4//
+let sum = 5 + 4;
+
+//alert window says 'Hello World!//
+function sayHello() {
+  alert('Hello World!');
+}
+
