f758a9ed50f521c7c1b615c3bcd4b1f6d52ba194
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-07-30 17:23:50 -0500

    added a function to check user's age

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ function sayHello() {
   alert('Hello World!');
 }
 
+function checkAge(name, age) {
+  if (age < 21) {
+    alert("Sorry "+ name + ", you are not old enough to view this page!")
+  }
+}
+
+checkAge('Charles', 21);
+checkAge('Abby', 27);
+checkAge('James', 18);
+checkAge('John', 17);
