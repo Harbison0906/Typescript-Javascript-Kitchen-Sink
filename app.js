2c1354f7e60822e4ab95b626fb95fe723215ac6e
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-02 22:36:28 -0500

    #10 of lab. Check-age function within a for loop

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ function sayHello() {
   alert('Hello World!');
 }
 
+sayHello();
+
+//alert window notifies if user is under 21//
 function checkAge(name, age) {
   if (age < 21) {
     alert("Sorry "+ name + ", you are not old enough to view this page!")
@@ -22,3 +25,25 @@ checkAge('Charles', 21);
 checkAge('Abby', 27);
 checkAge('James', 18);
 checkAge('John', 17);
+
+//display favorite vegetables in console//
+let favVeggies = ['green peppers', 'asparagus', 'onions', 'potatoes', 'squash'];
+
+let count = 0;
+
+while(count < favVeggies.length) {
+  console.log(favVeggies[count]);
+  count++;
+}
+
+let friends = [
+  {name: 'Ryan', age: 23},
+  {name: 'Justin', age: 20},
+  {name: 'Michael', age: 25},
+  {name: 'Aaron', age: 18},
+  {name: 'Jeremy', age: 30}
+];
+
+for(let i = 0; i < friends.length; i++) {
+  checkAge(friends[i].name, friends[i].age);
+}
